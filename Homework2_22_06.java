6b3f5206ff7eda03b1daa5c0e2a49ff308b3124e
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-23 22:11:25 +0300

    Add files via upload

---
+++ Homework2_22_06.java
@@ -0,0 +1,15 @@
+package org.telran.homeworks;
+
+public class Homework2_22_06 {
+    public static void main(String[] args) {
+
+    }
+    static void Digits() {
+        int myNum = 345;
+        int digit = myNum / 100;
+        int digit2 = (myNum / 10) % 10;
+        int digit3 = myNum % 10;
+        System.out.println(digit + "," + digit2 + "," + digit3);
+    }
+    }
+
