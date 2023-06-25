a52ba5a0d66a027ecd44416188a858a2b62e4e2f
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-25 22:54:46 +0300

    Update Homework2_22_06.java

--- Homework2_22_06.java
+++ Homework2_22_06.java
@@ -1,15 +1,11 @@
-package org.telran.homeworks;
-
-public class Homework2_22_06 {
+package org.example
+    
+    public class Home_22_06 {
     public static void main(String[] args) {
-
-    }
-    static void Digits() {
-        int myNum = 345;
-        int digit = myNum / 100;
-        int digit2 = (myNum / 10) % 10;
-        int digit3 = myNum % 10;
-        System.out.println(digit + "," + digit2 + "," + digit3);
-    }
-    }
-
+            int myNum = 345;
+            int digit = myNum / 100;
+            int digit2 = (myNum / 10) % 10;
+            int digit3 = myNum % 10;
+            System.out.println(digit +  ","  + digit2 + "," + digit3);
+        }
+}
