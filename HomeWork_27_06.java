c8eeb940f8c549f489c1718334e988dd2d80f849
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-30 22:50:37 +0300

    Update HomeWork_27_06.java

--- HomeWork_27_06.java
+++ HomeWork_27_06.java
@@ -2,16 +2,18 @@ package org.example;
 
 public class HomeWork_27_06 {
     public static void main(String[] args) {
+        String myStr = new String("I study Basic Java!");
+        System.out.println(myStr);
+        System.out.println(myStr.charAt(myStr.length() - 2));
 
-            String myStr = new String("I study Basic Java!");
-            System.out.println(myStr);
-            System.out.println(myStr.charAt(myStr.length() - 2));
+        System.out.println(myStr.contains("Java"));
+        System.out.println("I study Basic Java!".substring(0, 13));
+        System.out.println(myStr.replace("a", "o"));
+        System.out.println(myStr.toUpperCase());
+        System.out.println(myStr.toLowerCase());
+    }
+    private static void runStringMethod (String Str) {
 
-            System.out.println(myStr.contains("Java"));
-            System.out.println("I study Basic Java!".substring(0, 13));
-            System.out.println(myStr.replace("a", "o"));
-            System.out.println(myStr.toUpperCase());
-            System.out.println(myStr.toLowerCase());
         }
     }
 
