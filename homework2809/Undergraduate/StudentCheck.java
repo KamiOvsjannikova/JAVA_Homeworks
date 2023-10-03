888967b97387fe792b4c7795e28ecfbe6e5b7353
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-10-03 07:50:51 +0300

    Update StudentCheck.java

--- homework2809/Undergraduate/StudentCheck.java
+++ homework2809/Undergraduate/StudentCheck.java
@@ -2,17 +2,17 @@ import org.example.homework2809.Undergraduate.Learner.Student;
 
 public class StudentCheck {
     public static void main(String[] args) {
-        Student student = new Student("Max", "Login", 0);; //ukazivaju studenta
+        Student student = new Student("Max", "Login", 0);; //указываю студента
 
-        student.setGroupNumber(-5);// peredaju nomer.gruppi
+        student.setGroupNumber(-5);// номер группы передаю
 
         System.out.println("Student group number is: " + student.getGroupNumber());
 
         student.increaseGroupNumber();
         System.out.println("New group number is: " + student.getGroupNumber());
 
-        Student studentClone = new Student(student);//sozdaju nov.objekt i klon
-        System.out.println(student == studentClone);//proverka = ssilok
+        Student studentClone = new Student(student);// новый объект студента клона
+        System.out.println(student == studentClone);//проверка = ссылок
 
         System.out.println(student.getGroupNumber());
         System.out.println(studentClone.getGroupNumber());
