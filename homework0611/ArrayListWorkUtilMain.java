d4e42a0c455e4f4e274245c39eb8d6e22c065217
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-11-11 18:40:04 +0200

    Update ArrayListWorkUtilMain.java

--- homework0611/ArrayListWorkUtilMain.java
+++ homework0611/ArrayListWorkUtilMain.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class ArrayListWorkUtilMain {
     public static void main(String[] args) {
+        
+        ArrayListWorkUtil arrayListWorkUtil = new ArrayListWorkUtil();// ключик к нужному классу, чтобы понимать программе куда обращаться
+
         List<String> listOne = new ArrayList<>(); //добавляю элементы в список listOne
         listOne.add("This is "); //передаю строки
         listOne.add("my second");
@@ -18,8 +21,6 @@ public class ArrayListWorkUtilMain {
         List.add(5);
         System.out.println("Max value in the int list: " + ArrayListWorkUtil.findMax(List));
         System.out.println("Max length in the string list: " + ArrayListWorkUtil.maxLength(listOne));
-
-        ArrayListWorkUtil task = new ArrayListWorkUtil();// Ключик, создаю объект класса  ArrayListWorkUtil task  для использования его методов
 
         List<String> list_array1 = new ArrayList<>();//добавляю элементы в список list_array1
         list_array1.add("One"); //передаю строки
@@ -36,8 +37,8 @@ public class ArrayListWorkUtilMain {
         list_array2.add(87);
         list_array2.add(12);
 
-        System.out.println(task.test1(list_array1, 4));
-        System.out.println(task.test2(list_array2));
+        System.out.println(arrayListWorkUtil.test1(list_array1, 4));
+        System.out.println(arrayListWorkUtil.test2(list_array2));
 
     }
 }
